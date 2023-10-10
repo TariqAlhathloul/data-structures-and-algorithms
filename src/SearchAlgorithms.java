5d1121f53525e90ed3a7ec0f9607fff37a168041
Tariq Hassan <tariq_assan@Tariq.local>
2023-10-10 15:44:54 +0300

    added to my project

---
+++ src/SearchAlgorithms.java
@@ -0,0 +1,28 @@
+public class SearchAlgorithms {
+
+    public int binarySearch(int[] arr, int x) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (x == arr[mid]) {
+                return mid;
+            } else if (x > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public int linearSearch(int []arr, int index){
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == index){
+                System.out.println("element found at index [" + i + "] = " + arr[i]);
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/SortAlgorithms.java
@@ -0,0 +1,50 @@
+public class SortAlgorithms {
+    public void bubbleSort(int []arr) {
+        int i, j, temp, count=0;
+        //boolean swapped = false;
+
+        for( i = 0; i < arr.length-1; i++) {
+
+            for (j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    count++;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    //swapped = true;
+                }
+            }
+        }
+        printArr(arr,count);
+    }
+    public void InsertionSort(int []arr) {
+        /*
+        * array = {3,2,4,1,5}
+        * -------------------
+        * i.  pass  {2,3,4,1,5}
+        * ii. pass  {2,3,1,4,5}
+        * iii.pass  {2,1,3,4,5}
+        * iv. pass  {1,2,3,4,5}
+        * ------------------
+        * */
+        int count=0;
+        for(int i = 1; i < arr.length; ++i){
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                count++;
+            }
+            arr[j + 1] = key;
+        }
+        printArr(arr,count);
+    }
+    public void printArr(int []arr, int counter){
+        System.out.print("{");
+        for (int i : arr) {
+            System.out.print(i + ",");
+        }
+        System.out.println("} after: " + counter + " Passes.");
+    }
+}
